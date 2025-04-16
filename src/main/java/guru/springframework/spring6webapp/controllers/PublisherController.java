27afcae261b61491e4bf782a59a0f09e13c04ebd
Vishnu Tej Nanda Kumar <vishnutejnk7@gmail.com>
2025-04-16 00:10:25 -0300

    Add controllers for Author, Book and Publisher

---
+++ src/main/java/guru/springframework/spring6webapp/controllers/PublisherController.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring6webapp.controllers;
+
+import guru.springframework.spring6webapp.services.PublisherService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PublisherController {
+
+    private final PublisherService publisherService;
+
+    public PublisherController(PublisherService publisherService) {
+        this.publisherService = publisherService;
+    }
+
+    @RequestMapping("/publishers")
+    public String listPublishers(Model model) {
+        model.addAttribute("publishers", publisherService.findAll());
+        return "list-publishers";
+    }
+}
